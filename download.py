24d2d0aedaf4c4e9312b2ae62e78557055adf3ec
Xu <yxu209@weiran1-group.cs.uiowa.edu>
2026-08-11 14:45:28 -0500

    add download

---
+++ download.py
@@ -0,0 +1,14 @@
+import os
+
+# Replace with your actual intended download directory
+local_dir = "/data/not_backed_up/yxu209/models/qwen"
+
+# Make sure the parent folders exist
+os.makedirs(local_dir, exist_ok=True)
+
+from huggingface_hub import snapshot_download
+
+snapshot_download(
+    repo_id="Qwen/Qwen3.6-27B",
+    local_dir=local_dir,
+)
